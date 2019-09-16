8d432d08e9a174682f0988b2f8def753e53e161d
Wojciech Pluta <wojciech.pluta1@gmail.com>
2019-09-16 17:59:50 +0200

    Updates

---
+++ precache-manifest.81b8fce3000d5e5fe03c5aec5f4429b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5044a95eb5eeb494ae95cad6139da7",
+    "url": "/MapQuiz/index.html"
+  },
+  {
+    "revision": "940d37bfdb80f99954c0",
+    "url": "/MapQuiz/static/css/main.bdf4187a.chunk.css"
+  },
+  {
+    "revision": "66514f4cf39f3a22b885",
+    "url": "/MapQuiz/static/js/2.dc2dad63.chunk.js"
+  },
+  {
+    "revision": "940d37bfdb80f99954c0",
+    "url": "/MapQuiz/static/js/main.4b4a9def.chunk.js"
+  },
+  {
+    "revision": "fdc38815e7ba3269e5d3",
+    "url": "/MapQuiz/static/js/runtime~main.1bfd51f1.js"
+  },
+  {
+    "revision": "5eee6406cd60fde65eb14dd3848914cb",
+    "url": "/MapQuiz/static/media/main.5eee6406.jpg"
+  },
+  {
+    "revision": "718c0f54458e4c737f849de9456499cc",
+    "url": "/MapQuiz/static/media/mainEnd.718c0f54.jpg"
+  }
+]);
